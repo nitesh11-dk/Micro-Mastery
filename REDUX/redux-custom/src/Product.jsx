281b134248f4356d03b86c2ff56ad7440684ba3e
Nitesh-DK <nittesh.dk@gmail.com>
2024-09-30 12:02:10 +0530

    all async opertion on state is done

--- REDUX/redux-custom/src/Product.jsx
+++ REDUX/redux-custom/src/Product.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+// Product.js
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { asyncgetProdcut } from './appStore/actions/ProductActions';
-import { useSelector } from 'react-redux';
-const Product = () => {
-const dispatch = useDispatch();  
-const products = useSelector((state)=>state.product.items)
 
-useEffect(()=>{
-  // dispatch(getProduct());
-dispatch(  asyncgetProdcut())
-},[dispatch])
+const Product = () => {
+  const dispatch = useDispatch();  
+  const products = useSelector((state) => state.product.items);
+
+  useEffect(() => {
+    dispatch(asyncgetProdcut());
+  }, [dispatch]);
 
   return (
-    <div>
-{
-  products.map(product=>{
-    return (
-      <h1 key={product.id}>{product.id}  {product.title}</h1>
-    )
-  })
-}
+    <div className="flex flex-wrap justify-around p-5">
+      {products.map((product) => (
+        <div className="border border-gray-300 h-64 rounded-lg p-4 m-2 w-72 shadow-md hover:shadow-lg transition-shadow duration-200" key={product.id}>
+          <h2 className="text-lg font-bold mb-2">{product.title}</h2>
+          <p className="text-sm text-gray-600 mb-2">{product.description}</p>
+          <p className="text-xl text-green-600 mb-2">${product.price.toFixed(2)}</p>
+          <p className="text-xs text-gray-500">{product.category}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
