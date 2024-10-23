3b7043dcc4043cc555fbabd9c2cf8228e3aa7ed7
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-23 17:53:38 +0530

    private and public done

--- TS_Learning/app.ts
+++ TS_Learning/app.ts
@@ -3,6 +3,9 @@
 // •Class definition
 // •Constructors
 // •Access modifiers (public,
+// protected,
+// private)
+
 // •Readonly properties
 // // •Optional properties
 // •Parameter properties
@@ -31,14 +34,80 @@ interface User2 {
     age:number
 }
 class User {
-
     constructor(public name: string, public age: number,public detls:User2) {
-        this.name = name;
-        this.age = age; 
-        this.detls = detls
     } 
 }
 let user1 = new User("John", 30,{name:"hello", age:23});
-console.log(user1.detls);
+// console.log(user1.detls);
 // let user2 = new User("Nitesh", 19);
 // console.log(user2);
+
+
+//  ? this 
+
+// ! M -1
+// class Dog{
+// public name:string ;
+//     constructor(name:string){
+//        this.name= name ;
+//     }
+// }
+// M-2
+// class Dog{
+//     constructor(public name:string ){
+//     }
+// }
+
+// let dog = new Dog("hello");
+// console.log(dog.name);
+
+// ?  •Access modifiers (public,
+// ?  protected,
+// ?  private)
+
+
+//  public 
+//  if your class is public you can access it from anywhere , in class , out side the class in classs  methods and also in in inheritance class 
+// class Dog{
+//     public name:string ;
+//     constructor(name:string ){
+//           this.name= name ;
+//     }
+// }
+
+// class Cat extends Dog{
+//     constructor(name:string){
+//         super(name);
+//     }
+// }
+
+// let dog = new Dog("hello");
+// console.log(dog.name);
+
+// let cat = new Cat("kitty");
+// console.log(cat.name);
+
+// ? private 
+//  ts me error  dega but but js me vo convert hoga execute hoga  
+//  the attributes that are defined ussisng this  private keyword , we can aceest that varible in the class but not outside the class or in class instace or classs childs 
+// class Dog{
+//     private name:string ;
+//     constructor(name:string ){
+//           this.name= name ;
+//     }
+// }
+
+// class Cat extends Dog{
+//     constructor(name:string){
+//         super(name);
+//     }
+// }
+
+// let dog = new Dog("hello");
+// console.log(dog.name);
+
+// let cat = new Cat("kitty");
+// console.log(cat.name);
+
+// Property 'name' is private and only accessible within class 'Dog'.
+// 107 console.log(dog.name);
