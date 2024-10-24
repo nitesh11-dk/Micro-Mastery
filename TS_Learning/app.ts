d89a6a7c08e189533e424d9dc239aa825631e569
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-24 07:29:46 +0530

    getter setter parameter properties static memebrs is done

--- TS_Learning/app.ts
+++ TS_Learning/app.ts
@@ -7,7 +7,8 @@
 // private)
 
 // •Readonly properties
-// // •Optional properties
+
+// •Optional properties
 // •Parameter properties
 // •Getters and setters
 // •Static members
@@ -66,7 +67,7 @@ let user1 = new User("John", 30,{name:"hello", age:23});
 // ?  private)
 
 
-//  public 
+// ? public 
 //  if your class is public you can access it from anywhere , in class , out side the class in classs  methods and also in in inheritance class 
 // class Dog{
 //     public name:string ;
@@ -111,6 +112,120 @@ let user1 = new User("John", 30,{name:"hello", age:23});
 
 // Property 'name' is private and only accessible within class 'Dog'.
 // 107 console.log(dog.name);
+
+
+// ? proctected , yisme class me hii access kar sakte ho , aur yiske sub claeed me vhi access kar sakte ho but , bhar use nahi kar sakte with any insstance of parent class or instance of inhertied class 
+
+// Readonly  properties
+// agar appko class me agar koi attribute me value ko change hone  nahi dena hai to app readonly uses kar ssakte ho 
+
+class Rakesh {
+    constructor(public readonly name: string,) {
+    }
+    //  1. access modifier
+    //  2. readonly
+
+    //  changeName(){
+    //     this.name = "kushwaha";
+    //  }
+    // constructor( readonly public  name: string,) {
+    // } // gives error 
+    //  tum change kar sakte ho tum ts error dega and js run kar dega 
+}
+
+let r1 = new Rakesh("hello");
+// r1.changeName();
+// console.log(r1.name);
+
+
+//?  •Optional properties
+
+// class Tree{
+// constructor(public name?:string ,public gender?:string){
+// }
+
+// //  ? by using ? you can do optional properties 
+// }
+// let t1 = new Tree();
+// let t2 = new Tree( , "male");
+// console.log(t2);
+
+// class Tree {
+//     constructor({ name, gender }: { name?: string; gender?: string } = {}) {
+//       this.name = name;
+//       this.gender = gender;
+//     }
+  
+//     public name?: string;
+//     public gender?: string;
+//   }
+  
+//   let t1 = new Tree();
+//   let t2 = new Tree({ gender: "male" }); // Only passing gender
+//   console.log(t2);
+  
+//  
+
+// •Getters and setters
+// •Static members
+// private,
+// protected)
+// •Abstract classes and methods
+
+// ? •Parameter properties
+// class uCK {
+//     constructor(public name: string, public age: number) {
+//     }
+//     // public name: string, public age: number ye chiz ko hum parameter properties kahte hai , qkii hum yusko acces modoier ko paramete me hii defince karre with name and also with dtype 
+// }
+
+
+//?  •Getters and setters
+
+// getter yak yaise method vo varable ko access karke dedega 
+//  setter yaise method vo variable ko change kar dega 
+
+class UK{
+    constructor(public _name:string , public age:number){
+        //  _name : it is specialy that it means it is getter 
+    }
+
+    
+    get name(){
+        return this._name;
+    }
+//  agar apane setter nahi banaya hai but insstace value ko change karr hai to error dega ki the property is readonly property 
+
+    set name(name:string){
+        this._name = name;
+    }
+}
+
+let uk1 = new UK("hello",12);
+// console.log(uk1.name); 
+// uk1.name="kushwaha";
+// console.log(uk1.name);
+
+
+// •Static members
+// private,
+// protected)
+
+//  static members
+
+// class Sherry{
+//     static version:string ="1.0.0";
+//     constructor ( public versionins:string){
+//         // Sherry.version=this.version;
+//         this.versionins=Sherry.version;
+//     }
+// }
+// let s1 = new Sherry("1.0.1");
+// console.log(s1.versionins);
+
+
+
+
 
 
 
