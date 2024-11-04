56c705d39b19c82e9a1746bbb0df84189d409ff4
Nitesh-DK <nittesh.dk@gmail.com>
2024-11-04 16:55:15 +0000

    alldone

--- Next/Code-Chai-Next/Neat_With_Auth/next-auth/src/dbConfig/dbConfig.ts
+++ Next/Code-Chai-Next/Neat_With_Auth/next-auth/src/dbConfig/dbConfig.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async (): Promise<void> => {
     try {
+        // mongoose.connect(process.env.MONGO_URI!) // The ! is a TypeScript non-null assertion operator that tells TypeScript this value won't be null/undefined
         mongoose.connect(process.env.MONGO_URI!) // The ! is a TypeScript non-null assertion operator that tells TypeScript this value won't be null/undefined
        const connection = mongoose.connection;
 
