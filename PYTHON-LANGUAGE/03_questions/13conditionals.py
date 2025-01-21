31a281212604c296314c988765ba5746105a425d
Nitesh-DK <nittesh.dk@gmail.com>
2025-01-21 19:00:20 +0530

    Add new Python script 13conditionals.py demonstrating conditional statements and age-based pricing logic. The script includes examples of if-elif-else structures and a ternary operator for price determination based on age, enhancing understanding of control flow in Python.

---
+++ PYTHON-LANGUAGE/03_questions/13conditionals.py
@@ -0,0 +1,36 @@
+
+# user se input jab lete hai input ka use karke then vo inpur ks type string hota hai 
+# age = int(input("Enter your age : "))
+
+
+# if(age < 13):
+#     print("child")
+# elif (age >= 13 and age < 18):
+#     print("teen")
+# else:
+#     print("adult")
+
+# if age < 13:
+#     print("child")
+# elif age >= 13 and age < 18:
+#     print("teen")
+# else:
+#     print("adult")
+
+# ? 2 
+# !  price = 12 if age >=18 else 8 
+
+# age = 21 
+# day= "wednesday"
+# price = 12 if age >=18 else 8 
+# if(day == "wednesday"):
+#     price = price - 2
+# print(price)
+
+#  ? 3 
+# exit(); # yiske bad jo vhi likha hai vo run nahi hone wala hai 
+# print("hello world")
+
+#  ? 4 
+
+
